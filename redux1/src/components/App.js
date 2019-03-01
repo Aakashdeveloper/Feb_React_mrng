05e0e9a5b963b98c3500584a989c1dc9d382e4be
aakashDeveloper <ahanda205@gmail.com>
2019-03-01 07:45:41 +0530

    a

---
+++ redux1/src/components/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>Redux 1</div>
+        )
+    }
+}
+
+export default App;
---
+++ redux1/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+
+import App from './components/App';
+import reducer from './reducers';
+
+const createStoreWithMiddleWare = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleWare(reducer)}>
+        <App/>
+    </Provider>
+,document.querySelector('#root'));
